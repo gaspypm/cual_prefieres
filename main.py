a6470c744fcdaad771cee8fa1808c395ac8e09c6
gaspypm <gaspypm@gmail.com>
2023-12-21 08:41:27 -0300

    Add text for each option

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ from moviepy.audio.io.AudioFileClip import AudioFileClip
 from moviepy.editor import VideoFileClip, ImageClip, CompositeVideoClip, concatenate_audioclips
 import os
 import PIL
+from moviepy.video.VideoClip import TextClip
 
 voices = voices()
 random_voice = randint(0, len(voices)-1)
 
 # Manually load options
-first_option = input("Primera opción: ").lower()
-second_option = input("Segunda opción: ").lower()
+option1 = input("Primera opción: ")
+option2 = input("Segunda opción: ")
 
 # Generate the audio
 voice = generate(
-  text="¿Cuál prefieres?" + first_option + "                           o," + second_option,
+  text="¿Cuál prefieres? " + option1 + (" u, " if option2.startswith('o') else " o, ") + option2,
   voice=voices[random_voice],
   model="eleven_multilingual_v2"
 )
@@ -23,8 +24,8 @@ voice = generate(
 save(voice, "voice.mp3")
 
 # Get images
-downloader.download(first_option, limit=2, output_dir='images', adult_filter_off=False, force_replace=False)
-downloader.download(second_option, limit=2, output_dir='images', adult_filter_off=False, force_replace=False)
+downloader.download(option1, limit=2, output_dir='images', adult_filter_off=False, force_replace=False)
+downloader.download(option2, limit=2, output_dir='images', adult_filter_off=False, force_replace=False)
 
 # Add audio to video
 clips = []
@@ -34,17 +35,26 @@ template = VideoFileClip("template.mp4").subclip(0, voice.duration + 6)
 clips.append(template)
 
 # Add images to video
-image1_path = os.listdir("images/" + first_option)
-image1 = ImageClip("images/" + first_option + "/" + image1_path[0]).set_start(0).set_duration(voice.duration + 6)
+image1_path = os.listdir("images/" + option1)
+image1 = ImageClip("images/" + option1 + "/" + image1_path[0]).set_start(0).set_duration(voice.duration + 6)
 image1 = image1.resize((600, 600), PIL.Image.LANCZOS)
 image1 = image1.set_pos("top")
 clips.append(image1)
 
-image2_path = os.listdir("images/" + second_option)
-image2 = ImageClip("images/" + second_option + "/" + image2_path[0]).set_start(0).set_duration(voice.duration + 6)
+image2_path = os.listdir("images/" + option2)
+image2 = ImageClip("images/" + option2 + "/" + image2_path[0]).set_start(0).set_duration(voice.duration + 6)
 image2 = image2.resize((600, 600), PIL.Image.LANCZOS)
 image2 = image2.set_pos("bottom")
 clips.append(image2)
+
+# Add text to video
+text1 = TextClip(option1, fontsize=70, color='white', font="Arial")
+text1 = text1.set_position(("center", 0.32), relative=True).set_duration(voice.duration + 5)
+clips.append(text1)
+
+text2 = TextClip(option2, fontsize=70, color='white', font="Arial")
+text2 = text2.set_position(("center", 0.55), relative=True).set_duration(voice.duration + 5)
+clips.append(text2)
 
 audio = concatenate_audioclips([voice, clock])
 video = CompositeVideoClip(clips)
